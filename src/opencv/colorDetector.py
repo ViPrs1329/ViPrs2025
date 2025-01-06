f556612a7e4e22ebeffc1b5b1a2cbc5623a58de5
imartin-esc <imartin7132010@gmail.com>
2025-01-06 14:18:11 -0600

    Cleaned up the code
    deleted unused commented code

--- src/opencv/colorDetector.py
+++ src/opencv/colorDetector.py
@@ -28,18 +28,6 @@ while(1):
     red_upper = np.array([200,110,255], np.uint8) 
     red_mask = cv2.inRange(hsvFrame, red_lower, red_upper) 
 
-    # # Set range for green color and 
-    # # define mask 
-    # green_lower = np.array([25, 52, 72], np.uint8) 
-    # green_upper = np.array([102, 255, 255], np.uint8) 
-    # green_mask = cv2.inRange(hsvFrame, green_lower, green_upper) 
-
-    # # Set range for blue color and 
-    # # define mask 
-    # blue_lower = np.array([94, 80, 2], np.uint8) 
-    # blue_upper = np.array([120, 255, 255], np.uint8) 
-    # blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper) 
-    
     # Morphological Transform, Dilation 
     # for each color and bitwise_and operator 
     # between imageFrame and mask determines 
@@ -50,16 +38,6 @@ while(1):
     red_mask = cv2.dilate(red_mask, kernel) 
     res_red = cv2.bitwise_and(imageFrame, imageFrame, 
                             mask = red_mask) 
-    
-    # # For green color 
-    # green_mask = cv2.dilate(green_mask, kernel) 
-    # res_green = cv2.bitwise_and(imageFrame, imageFrame, 
-    #                             mask = green_mask) 
-    
-    # # For blue color 
-    # blue_mask = cv2.dilate(blue_mask, kernel) 
-    # res_blue = cv2.bitwise_and(imageFrame, imageFrame, 
-    #                         mask = blue_mask) 
 
     # Creating contour to track red color 
     contours, hierarchy = cv2.findContours(red_mask, 
@@ -73,21 +51,14 @@ while(1):
           contour, cv2.arcLength(contour, True), True) 
         if (area > 300): 
             areasDict.append({"area": area, "rect": cv2.boundingRect(contour)})
-            # x, y, w, h = cv2.boundingRect(contour) 
-            # imageFrame = cv2.rectangle(imageFrame, (x, y), 
-            #                         (x + w, y + h), 
-            #                         (0, 0, 255), 2) 
-            
-            # cv2.putText(imageFrame, "Red Colour", (x, y), 
-            #             cv2.FONT_HERSHEY_SIMPLEX, 1.0, 
-            #             (0, 0, 255))     
+   
     def keyVal(item):
         # x, y, w, h = item["rect"]
         # center = x + w / 2
         # offset = abs(center - webcamWidth)
         # return offset
         return item["area"]
-    print(len(areasDict))
+
     if len(areasDict) != 0:
       areasDict = sorted(areasDict, key=keyVal)
       x, y, w, h = areasDict[-1]["rect"]
@@ -97,38 +68,6 @@ while(1):
       cv2.putText(imageFrame, "Red Colour", (x, y), 
                   cv2.FONT_HERSHEY_SIMPLEX, 1.0, 
                   (0, 0, 255))   
-    # Creating contour to track green color 
-    # contours, hierarchy = cv2.findContours(green_mask, 
-    #                                     cv2.RETR_TREE, 
-    #                                     cv2.CHAIN_APPROX_SIMPLE) 
-    
-    # for pic, contour in enumerate(contours): 
-    #     area = cv2.contourArea(contour) 
-    #     if(area > 300): 
-    #         x, y, w, h = cv2.boundingRect(contour) 
-    #         imageFrame = cv2.rectangle(imageFrame, (x, y), 
-    #                                 (x + w, y + h), 
-    #                                 (0, 255, 0), 2) 
-            
-    #         cv2.putText(imageFrame, "Green Colour", (x, y), 
-    #                     cv2.FONT_HERSHEY_SIMPLEX, 
-    #                     1.0, (0, 255, 0)) 
-
-    # Creating contour to track blue color 
-    # contours, hierarchy = cv2.findContours(blue_mask, 
-    #                                     cv2.RETR_TREE, 
-    #                                     cv2.CHAIN_APPROX_SIMPLE) 
-    # for pic, contour in enumerate(contours): 
-    #     area = cv2.contourArea(contour) 
-    #     if(area > 300): 
-    #         x, y, w, h = cv2.boundingRect(contour) 
-    #         imageFrame = cv2.rectangle(imageFrame, (x, y), 
-    #                                 (x + w, y + h), 
-    #                                 (255, 0, 0), 2) 
-            
-    #         cv2.putText(imageFrame, "Blue Colour", (x, y), 
-    #                     cv2.FONT_HERSHEY_SIMPLEX, 
-    #                     1.0, (255, 0, 0)) 
             
     # Program Termination 
     cv2.imshow("Multiple Color Detection in Real-TIme", imageFrame) 
