c3fadf808f20f3769c6f56903de785f301e1a928
imartin-esc <imartin7132010@gmail.com>
2025-01-10 09:30:59 -0600

    Added the driver code for swerve drive into robot.py

--- src/robot.py
+++ src/robot.py
@@ -7,18 +7,55 @@
 import time
 import wpilib
 import wpilib.drive
+from wpimath.kinematics import ChassisSpeeds
+from wpimath.geometry import Rotation2d
 import rev
+import math
 import commands2
-
+from subsystems.SwerveDriveSubsystem import DriveTrain
 
 class MyRobot(commands2.TimedCommandRobot):
+  def systemTempCheck(self):
+    motorControllers = [
+      self.drivetrain.frontLeftDrive,
+      self.drivetrain.frontRightDrive,
+      self.drivetrain.backLeftDrive,
+      self.drivetrain.backRightDrive,
+      self.drivetrain.backLeftRotation,
+      self.drivetrain.backRightRotation,
+      self.drivetrain.frontLeftRotation,
+      self.drivetrain.frontRightRotation,
+      self.drivetrain.intake.intakeRotation,
+      self.drivetrain.intake.intakeDrive,
+      self.shooter.shooterDrive1,
+      self.shooter.shooterDrive2,
+      self.shooter.feedMotor,
+      self.shooter.rotationMotor,
+      self.climber.leftClimber,
+      self.climber.rightClimber
+    ]
+
+    burntFlag = False
+    for motorController in motorControllers:
+      temp = motorController.getMotorTemperature()
+      if temp > 90:
+        print(f"[x] Motor {motorController.getDeviceId()}, {temp}C")
+        burntFlag = True
+      else:
+        print(f"[-] Motor {motorController.getDeviceId()}, {temp}C")
+    
+    if burntFlag:
+      self.startRumble()
+      for i in range(100):
+        print("!!! ---------------- MOTORS TOO HOT ------------------- !!!")
+        
   def startRumble(self):
-    self.xboxController.setRumble(self.xboxController.RumbleType.kRightRumble,1)
-    self.xboxController.setRumble(self.xboxController.RumbleType.kLeftRumble,1)
+    self.drivingXboxController.setRumble(self.drivingXboxController.RumbleType.kRightRumble,1)
+    self.drivingXboxController.setRumble(self.drivingXboxController.RumbleType.kLeftRumble,1)
 
   def stopRumble(self):
-    self.xboxController.setRumble(self.xboxController.RumbleType.kRightRumble,0)
-    self.xboxController.setRumble(self.xboxController.RumbleType.kLeftRumble,0)
+    self.drivingXboxController.setRumble(self.drivingXboxController.RumbleType.kRightRumble,0)
+    self.drivingXboxController.setRumble(self.drivingXboxController.RumbleType.kLeftRumble,0)
 
   autonomousCommand = None
   def robotInit(self):
@@ -26,6 +63,8 @@ class MyRobot(commands2.TimedCommandRobot):
     This function is called upon program startup and
     should be used for any initialization code.
     """
+    self.drivetrain = DriveTrain()
+
     print("robotInit()")
 
   def robotPeriodic(self):
@@ -35,13 +74,10 @@ class MyRobot(commands2.TimedCommandRobot):
   def autonomousInit(self):
     """This function is run once each time the robot enters autonomous mode."""
     print("autonomousInit()")
-        
-    pass
 
   def autonomousPeriodic(self):
     """This function is called periodically during autonomous."""
     print("autonomousPeriodic()")
-
     pass
 
   def disabledInit(self):
@@ -54,11 +90,37 @@ class MyRobot(commands2.TimedCommandRobot):
   def teleopInit(self): 
     """This function is called once each time the robot enters teleoperated mode."""
     print("teleopInit()")
-        
+    self.stopRumble()
+    self.drivetrain.resetHarder()
+    self.drivingXboxController = wpilib.XboxController(0)
+    self.systemTempCheck()
         
   def teleopPeriodic(self):
     """This function is called periodically during teleoperated mode."""
     print("teleopPeriodic()")
+    xSpeed = self.drivingXboxController.getLeftX()
+    ySpeed = self.drivingXboxController.getLeftY()
+    tSpeed = self.drivingXboxController.getRightX()
+
+    if abs(xspeed) <.10:
+      xspeed=0
+    if abs(yspeed) <.10:
+      yspeed=0
+    if abs(tspeed) <.10:
+      tspeed=0
+
+    yaw = self.drivetrain.gyro.get_yaw().value_as_double
+
+    h = yaw % 360
+    if h < 0:
+      h += 360
+
+    h2 = h / 360
+
+    heading = h2 * (math.pi * 2)
+
+    speeds = ChassisSpeeds.fromFieldRelativeSpeeds(xspeed, yspeed, -tspeed, Rotation2d(heading))
+    self.drivetrain.manualDriveFromChassisSpeeds(speeds)
         
 
   def testInit(self): 
