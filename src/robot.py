fee7317d81927c30d1ddac59bc98fc53a2aa4d65
imartin-esc <imartin7132010@gmail.com>
2025-01-18 12:46:13 -0600

    fixed the current vs incoming changes
    Normalized magnitude of driving to prevent faster cardinal driving

--- src/robot.py
+++ src/robot.py
@@ -43,77 +43,9 @@ class MyRobot(commands2.TimedCommandRobot):
     def disabledPeriodic(self):
         pass
 
-<<<<<<< Updated upstream
     def teleopInit(self): 
         """This function is called once each time the robot enters teleoperated mode."""
         print("teleopInit()")
-=======
-  def autonomousPeriodic(self):
-    """This function is called periodically during autonomous."""
-    print("autonomousPeriodic()")
-    pass
-
-  def disabledInit(self):
-    """This function is called initially when disabledd"""
-    print("disabledInit()")
-
-  def disabledPeriodic(self):
-    pass
-
-  def teleopInit(self): 
-    """This function is called once each time the robot enters teleoperated mode."""
-    print("teleopInit()")
-    self.stopRumble()
-    self.drivetrain.resetHarder()
-    self.systemTempCheck()
-
-  def inputCurve(input: float):
-    return (input ** 3)
-
-  def tinputCurve(input: float):
-    return (input ** 3) * constants.controller.tscale
-
-  def distanceCorrectedInputCurve(x: float, y: float):
-    d = math.sqrt(x * x + y * y)
-    #print(x)
-    #print(y)
-    #print(d)
-    s = MyRobot.inputCurve(d)
-    return x * s * constants.controller.scale, y * s * constants.controller.scale
-  
-  def teleopPeriodic(self):
-    """This function is called periodically during teleoperated mode."""
-    print("teleopPeriodic()")
-    xSpeed, ySpeed = MyRobot.distanceCorrectedInputCurve(self.drivingXboxController.getLeftY(), self.drivingXboxController.getLeftX())
-    # xSpeed = MyRobot.inputCurve(self.drivingXboxController.getLeftY())
-    # ySpeed = MyRobot.inputCurve(self.drivingXboxController.getLeftX())
-    tSpeed = MyRobot.tinputCurve(-self.drivingXboxController.getRightX())
-
-    if abs(xSpeed) < constants.controller.XYdeadzone:
-      xSpeed=0
-    if abs(ySpeed) < constants.controller.XYdeadzone:
-      ySpeed=0
-    if abs(tSpeed) < constants.controller.Tdeadzone:
-      tSpeed=0
-
-    yaw = self.drivetrain.gyro.get_yaw().value_as_double
-
-    h = yaw % 360
-    if h < 0:
-      h += 360
-
-    h2 = h / 360
-
-    heading = h2 * (math.pi * 2)
-
-    speeds = ChassisSpeeds.fromFieldRelativeSpeeds(xSpeed, ySpeed, -tSpeed, Rotation2d(heading))
-    self.drivetrain.manualDriveFromChassisSpeeds(speeds)
-        
-
-  def testInit(self): 
-    """This function is called once each time the robot enters test mode."""
-    print("testInit()")
->>>>>>> Stashed changes
         
         
     def teleopPeriodic(self):
