0c065c54a1fa38c058555f7383e0a57c3810796e
imartin-esc <imartin7132010@gmail.com>
2025-03-01 19:04:01 -0600

    corrected everything to 2 space tabs

--- src/commands/setElevator.py
+++ src/commands/setElevator.py
@@ -5,52 +5,52 @@ import wpilib
 import constants
 
 def getElevatorLevel(pressedButtons):
-    match pressedButtons:
-        case [False, False, False, True]:
-            pass # L1
-        case [False, False, True, False]:
-            pass # L1
-        case [False, True, False, False]:
-            pass # L1
-        case [True, False, False, False]:
-            pass # L1
-        case [True, True, False, False]:
-            pass # L2
-        case [True, False, True, False]:
-            pass # base
-        case [True, False, False, True]:
-            pass # L2
-        case [False, True, True, False]:
-            pass # base
-        case [False, True, False, True]:
-            pass # L2
-        case [False, False, True, True]:
-            pass # L2
-        case [True, True, True, False]:
-            pass # L3
-        case [True, True, False, True]:
-            pass # L3
-        case [True, False, True, True]:
-            pass # L3
-        case [False, True, True, True]:
-            pass # L3
-        case [True, True, True, True]:
-            pass # L4
+  match pressedButtons:
+    case [False, False, False, True]:
+      pass # L1
+    case [False, False, True, False]:
+      pass # L1
+    case [False, True, False, False]:
+      pass # L1
+    case [True, False, False, False]:
+      pass # L1
+    case [True, True, False, False]:
+      pass # L2
+    case [True, False, True, False]:
+      pass # base
+    case [True, False, False, True]:
+      pass # L2
+    case [False, True, True, False]:
+      pass # base
+    case [False, True, False, True]:
+      pass # L2
+    case [False, False, True, True]:
+      pass # L2
+    case [True, True, True, False]:
+      pass # L3
+    case [True, True, False, True]:
+      pass # L3
+    case [True, False, True, True]:
+      pass # L3
+    case [False, True, True, True]:
+      pass # L3
+    case [True, True, True, True]:
+      pass # L4
     # TODO finish the match cases
 
 class SetElevator(commands2.Command):
-    def __init__(self, pressed):
-        super().__init__()
-        self.buttons = pressed
+  def __init__(self, pressed):
+    super().__init__()
+    self.buttons = pressed
 
-    def initialize(self):
-        self.buttons[2] = True
+  def initialize(self):
+    self.buttons[2] = True
 
-    def execute(self):
-        pass
+  def execute(self):
+    pass
 
-    def end(self, interrupted: bool):
-        self.buttons[2] = False
+  def end(self, interrupted: bool):
+    self.buttons[2] = False
         
-    def isFinished(self) -> bool:
-        return True
+  def isFinished(self) -> bool:
+    return True
