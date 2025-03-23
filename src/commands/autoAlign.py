dcb5b5e35d780efb7eea534397fcca400b457f46
imartin-esc <imartin7132010@gmail.com>
2025-03-23 16:51:48 -0500

    now the values get posted to network tables for easier debugging

--- src/commands/autoAlign.py
+++ src/commands/autoAlign.py
@@ -6,10 +6,15 @@ from wpimath.controller import PIDController
 from wpimath.kinematics import ChassisSpeeds
 import constants
 import math
+import ntcore
 
 class AutoAlign(commands2.Command):
   def __init__(self, llSubsystem: LimelightSubsystem, drivetrain: DriveTrain, alignLocation: str):
     super().__init__()
+
+    inst = ntcore.NetworkTableInstance.getDefault()
+    self.table = inst.getTable("Auto Align")
+
     self.llSubsystem = llSubsystem
     self.drivetrain = drivetrain
     self.alignPosition = 0
@@ -19,6 +24,14 @@ class AutoAlign(commands2.Command):
       self.alignPosition = -constants.visionConsts.alignOffset
     else:
       raise ValueError(f"robot can't align to {alignLocation}. must be 'left' or 'right'")
+    
+    self.alignLocationXPub = self.table.getDoubleTopic("Align Location X").publish()
+    self.alignLocationYPub = self.table.getDoubleTopic("Align Location Y").publish()
+    self.alignLocationTPub = self.table.getDoubleTopic("Align Location T").publish()
+    self.currentXPub = self.table.getDoubleTopic("Current X").publish()
+    self.currentYPub = self.table.getDoubleTopic("Current Y").publish()
+    self.currentTPub = self.table.getDoubleTopic("Current T").publish()
+
     xkp = 0.3
     xki = 0
     xkd = 0
@@ -28,14 +41,18 @@ class AutoAlign(commands2.Command):
     ykd = 0
     self.xController = PIDController(xkp, xki, xkd)
     self.xController.setSetpoint(self.alignPosition)
+    self.alignLocationXPub.set(self.alignPosition)
+
     self.yController = PIDController(ykp, yki, ykd)
     self.yController.setSetpoint(0)
+    self.alignLocationYPub.set(0)
     
     tkp = 10
     tki = 0
     tkd = 0
     self.tController = PIDController(tkp, tki, tkd)
     self.tController.setSetpoint(0)
+    self.alignLocationTPub.set(0)
 
     self.dx = self.dy = self.dt = 1000
 
@@ -49,8 +66,13 @@ class AutoAlign(commands2.Command):
       self.dx = targetPose.X()
       self.dy = targetPose.Y()
       self.dz = targetPose.Z()
+
       self.dt = targetPose.rotation().Z()
 
+      self.currentXPub.set(self.dx)
+      self.currentYPub.set(self.dy)
+      self.currentTPub.set(self.dt)
+      
       xSpeed = self.xController.calculate(self.dx)
       ySpeed = self.yController.calculate(self.dy)
       tSpeed = -self.tController.calculate(self.dt)
