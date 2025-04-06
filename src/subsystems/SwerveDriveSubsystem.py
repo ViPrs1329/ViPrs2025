7af43cc1656dd47f079256c1e8a051c0f230a9b8
imartin-esc <imartin7132010@gmail.com>
2025-04-06 08:33:26 -0500

    better implementation of relative drive from claude

--- src/subsystems/SwerveDriveSubsystem.py
+++ src/subsystems/SwerveDriveSubsystem.py
@@ -416,14 +416,53 @@ class DriveTrain(commands2.Subsystem):
     # print('\n')
 
   def driveFromRelativeCoordinates(self, vx: float, vy: float, vt: float):
+    """
     rotation = degreesToRadians(self.gyro.get_yaw().value_as_double)
     deltax = vx * math.cos(rotation) + vy * math.sin(rotation)
     deltay = vx * math.sin(rotation) - vy * math.cos(rotation)
     speeds = ChassisSpeeds(deltax, deltay, vt)
     self.manualDriveFromChassisSpeeds(speeds)
+    """
     # robotRelativeSpeeds = ChassisSpeeds(vx, vy, vt)
     # targetSpeeds = robotRelativeSpeeds.discretize()
     # targetStates = self.kinematics.toSwerveModuleStates(targetSpeeds)
+    # Get the current robot heading from the gyro
+    currentAngle = self.getGyroHeading()
+    
+    # Create field-relative chassis speeds
+    fieldRelativeSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(
+        vx, vy, vt, currentAngle
+    )
+    
+    # Convert to module states
+    moduleStates = self.kinematics.toSwerveModuleStates(fieldRelativeSpeeds)
+    
+    # Desaturate wheel speeds
+    maxSpeed = 4.1  # Same as in your other method
+    frontLeft, frontRight, backLeft, backRight = SwerveDrive4Kinematics.desaturateWheelSpeeds(
+        moduleStates, maxSpeed
+    )
+    
+    # Optimize and set module states directly
+    frontLeft.optimize(Rotation2d(ticks2rad(self.FleftEnc.get_absolute_position()._value)))
+    frontRight.optimize(Rotation2d(ticks2rad(self.FrightEnc.get_absolute_position()._value)))
+    backLeft.optimize(Rotation2d(ticks2rad(self.BleftEnc.get_absolute_position()._value)))
+    backRight.optimize(Rotation2d(ticks2rad(self.BrightEnc.get_absolute_position()._value)))
+    
+    # Set rotation motors
+    self.backLeftRotation.set(-self.BleftPID.calculate(self.BleftEnc.get_absolute_position()._value, lratio(backLeft.angle.radians())))
+    self.frontLeftRotation.set(-self.FleftPID.calculate(self.FleftEnc.get_absolute_position()._value, lratio(frontLeft.angle.radians())))
+    self.backRightRotation.set(-self.BrightPID.calculate(self.BrightEnc.get_absolute_position()._value, lratio(backRight.angle.radians())))
+    self.frontRightRotation.set(-self.FrightPID.calculate(self.FrightEnc.get_absolute_position()._value, lratio(frontRight.angle.radians())))
+    
+    # Set drive motors
+    self.backLeftDrive.set(backLeft.speed)
+    self.backRightDrive.set(backRight.speed)
+    self.frontLeftDrive.set(frontLeft.speed)
+    self.frontRightDrive.set(frontRight.speed)
+    
+    # Update lastChassisSpeed for odometry
+    self.lastChassisSpeed = fieldRelativeSpeeds
     
   def driveFromChassisSpeeds(self, speeds: ChassisSpeeds) -> None: #not used in current robot.py implementation as of 2/28
     self.lastChassisSpeed = speeds
