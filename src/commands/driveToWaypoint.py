106bde56b78fc7fa5fbd8c7b9d401d0ce9eee05f
imartin-esc <imartin7132010@gmail.com>
2025-04-10 21:41:18 -0500

    implemented the waypoint subsystem logic
    the waypoint subsystem now has the ability to add waypoints and commands that it calls in order and automatically resets the odometry if given a starting pose.

---
+++ src/commands/driveToWaypoint.py
@@ -0,0 +1,78 @@
+import commands2
+import wpilib
+from subsystems.LimelightSubsystem import LimelightSubsystem
+from subsystems.SwerveDriveSubsystem import DriveTrain
+from wpimath.controller import PIDController
+from wpimath.kinematics import ChassisSpeeds
+from wpimath.geometry import Pose2d
+import constants
+import math
+import ntcore
+
+class DriveToWaypoint(commands2.Command):
+  def __init__(self, drivetrain: DriveTrain, odometrySupplier: callable, waypoint: Pose2d, precisionXY: float, precisionT: float):
+    super().__init__()
+
+    inst = ntcore.NetworkTableInstance.getDefault()
+    self.table = inst.getTable("Waypoint")
+
+    self.drivetrain = drivetrain
+    self.odSupplier: callable = odometrySupplier
+    self.targetLoc = waypoint
+
+    self.targetPub = self.table.getStructTopic("target pose", Pose2d).publish()
+
+    self.precisionXY = precisionXY
+    self.precisionT = precisionT
+
+  def initialize(self):
+
+    xkp = 0.3
+    xki = 0.0
+    xkd = 0.0
+
+    ykp = 0.3
+    yki = 0.0
+    ykd = 0.0
+    self.xController = PIDController(xkp, xki, xkd)
+    self.xController.setSetpoint(self.targetLoc.X())
+
+    self.yController = PIDController(ykp, yki, ykd)
+    self.yController.setSetpoint(self.targetLoc.Y())
+    
+    tkp = 0.5
+    tki = 0.0
+    tkd = 0.0
+    self.tController = PIDController(tkp, tki, tkd)
+    self.tController.setSetpoint(self.targetLoc.rotation().radians())
+
+    self.dx = self.dy = self.dt = 1000
+
+  def execute(self):
+    odometry: Pose2d = self.odSupplier()
+    self.dx = odometry.X()
+    self.dy = odometry.Y()
+
+    self.dt = odometry.rotation().radians()
+    
+    xSpeed = self.xController.calculate(self.dx)
+    ySpeed = self.yController.calculate(self.dz)
+    tSpeed = self.tController.calculate(self.dt)
+
+    speeds = ChassisSpeeds(xSpeed, ySpeed, tSpeed)
+    self.drivetrain.manualDriveFromChassisSpeeds(speeds)
+    
+  def end(self, interrupted: bool):
+    pass
+
+  def inTollerance(self):
+    if (abs(self.dx - self.xController.getSetpoint()) < self.precisionXY) and (abs(self.dy - self.yController.getSetpoint()) < self.precisionXY) and (abs(self.dt) < self.precisionT):
+      return True
+    else:
+      return False
+    
+  def isFinished(self) -> bool:
+    if self.inTollerance():
+      print("bang ding ow (out toller)")
+      return True
+    return False
--- src/subsystems/WaypointSubsystem.py
+++ src/subsystems/WaypointSubsystem.py
@@ -1,9 +1,46 @@
 import commands2, constants
 import wpilib
+from wpimath.geometry import Pose2d, Rotation2d
+from commands.driveToWaypoint import DriveToWaypoint
+from subsystems.SwerveDriveSubsystem import DriveTrain
 
 class Waypoint(commands2.Subsystem):
-  def __init__(self) -> None:
+  def __init__(self, drivetrain: DriveTrain) -> None:
     super().__init__()
+    self.queue: list[commands2.Command] = []
+    self.drivetrain = drivetrain
+    self.odSupplier: callable = drivetrain.getPose
+    self.startingPoint = Pose2d(0, 0, Rotation2d())
+    self.resetOdometry = drivetrain.resetOdometry
+
+  def addWaypoint(self, waypoint: Pose2d, precisionXY: float = 0.05, precisionT: float = 0.1):
+    self.queue.append(
+      DriveToWaypoint(
+        self.drivetrain,
+        self.odSupplier,
+        waypoint,
+        precisionXY,
+        precisionT
+      )
+    )
+
+  def addCommand(self, command: commands2.Command):
+    self.queue.append(command)
+
+  def setStartingPose(self, startingPoint: Pose2d):
+    self.startingPoint = startingPoint
+
+  def getStartingPose(self):
+    return self.startingPoint
+
+  def getAutonomousCommand(self):
+    # the "*" unpacks a list into seperate arguments
+    return commands2.SequentialCommandGroup(
+      commands2.InstantCommand(
+        lambda: self.resetOdometry(self.startingPoint)
+      ),
+      *self.queue
+    ) 
 
   def periodic(self):
     pass
