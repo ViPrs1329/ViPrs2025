ffab9902e181f5ab5dad8fc2cbecb132d9856613
imartin-esc <imartin7132010@gmail.com>
2025-04-11 15:41:08 -0500

    Revert "Restored"
    This reverts commit 80346e6bc120eba0b7449c9a504518b1dcba5d49.

--- src/subsystems/SwerveDriveSubsystem.py
+++ src/subsystems/SwerveDriveSubsystem.py
@@ -432,7 +432,7 @@ class DriveTrain(commands2.Subsystem):
 
     self.lastChassisSpeed = speeds
 
-    Vx = speeds.vy  
+    Vx = speeds.vy
     Vy = speeds.vx
     
     speeds = ChassisSpeeds(-Vx, -Vy, -speeds.omega)
