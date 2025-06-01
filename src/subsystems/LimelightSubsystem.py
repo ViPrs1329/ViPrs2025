bf597a520376ae2a0a5cad3d1c08fde5e2d9758d
imartin-esc <imartin7132010@gmail.com>
2025-06-01 11:12:11 -0500

    added relative robot position in target space

--- src/subsystems/LimelightSubsystem.py
+++ src/subsystems/LimelightSubsystem.py
@@ -14,7 +14,7 @@ class LimelightSubsystem(Subsystem):
         
         self.tables: list[NetworkTable] = []
         for name in Limelight.Consts.tableNames:
-            table = NetworkTableInstance.getDefault().getTable(name)
+            table: NetworkTable = NetworkTableInstance.getDefault().getTable(name)
             self.tables.append(table)
 
     def canSeeTarget(self) -> bool:
@@ -32,14 +32,45 @@ class LimelightSubsystem(Subsystem):
         
         robotPose: Pose2d = Pose2d(0, 0, Rotation2d(0))
 
+        count: int = 0
+        # Iterate through all tables to accumulate the robot's position
         for table in self.tables:
             
             # botpose_orb uses MT2 compared to botpose which uses MT1
             botPose = table.getNumberArray("botpose_orb", [0, 0, 0, 0, 0, 0]) 
             if table.getNumber("tv", 0) == 1:
+                count += 1
                 x = botPose[0]
                 y = botPose[1]
                 rotation = Rotation2d.fromDegrees(botPose[5])
                 robotPose += Transform2d(x, y, rotation) # yes, this is a Transform2d, not a Pose2d
         
+        robotPose /= count if count > 0 else 1  # Average the position if multiple positions are used
+        return robotPose
+    
+    def getRobotPositionRelativeToTarget(self, target: int) -> Pose2d:
+        """
+        Get the robot's position relative to the specified targets.
+        
+        :param targets: List of target indices to consider.
+        :return: Pose2d representing the robot's position relative to the targets.
+        """
+        if not self.canSeeTarget():
+            return Pose2d(0, 0, Rotation2d(0))
+        
+        robotPose: Pose2d = Pose2d(0, 0, Rotation2d(0))
+
+        count: int = 0
+
+        # Iterate through all tables to accumulate the robot's position
+        for table in self.tables:
+            if table.getNumber("tv", 0) == 1 and table.getNumber("tid", -1) == target:
+                count += 1
+                botPose = table.getNumberArray("botpose_targetspace", [0, 0, 0, 0, 0, 0])
+                x = botPose[0]
+                z = botPose[2]
+                rotation = Rotation2d.fromDegrees(botPose[5])
+                robotPose += Transform2d(x, z, rotation)
+
+        robotPose /= count if count > 0 else 1  # Average the position if multiple positions are used
         return robotPose
